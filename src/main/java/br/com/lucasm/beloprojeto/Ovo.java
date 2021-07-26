87776172e05b52057a98fb541a20a7a00b5a7fec
Lucas Martins Gonçalves <lucaasss14@gmail.com>
2021-07-26 11:54:39 -0300

    commit

--- src/main/java/br/com/lucasm/beloprojeto/Ovo.java
+++ src/main/java/br/com/lucasm/beloprojeto/Ovo.java
@@ -2,10 +2,22 @@ package br.com.lucasm.beloprojeto;
 
 public class Ovo {
 
-	private Integer frito;
-	
-	public Ovo(Integer minFrit) {
-		this.frito = minFrit;
+	private Integer tamanho;
+	private String qualidade;  //construtor
+
+	public Ovo(Integer tamanho) {
+		this.tamanho = tamanho;
 	}
-	
+
+	public void fritar() {
+
+	}
+
+	public Integer getTamanho(){
+		return tamanho;
+	}
+
+	public String getQualidade(){
+		return qualidade;
+	}
 }
